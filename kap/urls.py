da7cac5791d5be372ec04767a905abd663d26d3a
Unknown <g.r.nobles@rug.nl>
2019-01-14 00:05:32 +0300

    chat urls added

--- kap/urls.py
+++ kap/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from chat.views import index
+
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 
-
+    path('chat/', include('chat.urls', namespace='chat')),
 
     path('', jobs.views.home, name='home'),
     path('blog/', include('blog.urls')),
@@ -18,7 +20,8 @@ urlpatterns = [
 
     #Zooarch
     path('zooarch/',include('zooarch.urls')),
-    #path('zooarch/', include('zooarch.urls')),
+    #experimental
+    path('experimental/', include('experimental.urls')),
 
     path('spatial3d/', include('spatial3d.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
