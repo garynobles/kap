4d89b2c50e1bc47d59f6d0e377834574d55f6e39
Unknown <g.r.nobles@rug.nl>
2019-01-14 00:43:05 +0300

    chat model setup

---
+++ chat/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextFirld()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
