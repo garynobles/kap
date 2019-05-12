e9b7a2323eddf5d5e8677f38e1e7fa758304d5fa
Gary <garynobles20@gmail.com>
2019-05-12 03:42:29 +0300

    tickets update

--- tickets/models.py
+++ tickets/models.py
@@ -13,6 +13,7 @@ DEPT_CHOICES = (
     ("Botany","Botany"),
     ("Ceramics","Ceramics"),
     ("Conservation","Conservation"),
+    ("Database","Database"),
     ("Excavation","Excavation"),
     ("Zooarch","Zooarchaeology"),
     ("Other","Other")
@@ -41,13 +42,13 @@ PRIORITY_CHOICES = (
 # Create your models here.
 class Ticket(models.Model):
     ticket_id = models.AutoField(primary_key=True)
-    submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL, db_column='taken_by', blank=True, null=True, on_delete = models.PROTECT)
-    subject = models.CharField(max_length=200, default='', blank=True, null=True)
-    details = models.CharField(max_length=5000, default='', blank=True, null=True)
-    category = models.CharField(max_length=20, default='', choices = CAT_CHOICES, blank=True, null=True)
-    system = models.CharField(max_length=20, default='', choices = SYS_CHOICES, blank=True, null=True)
+    submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL, db_column='taken_by', on_delete = models.PROTECT)
+    subject = models.CharField(max_length=200, default='')
+    details = models.CharField(max_length=5000, default='')
+    category = models.CharField(max_length=20, default='', choices = CAT_CHOICES)
+    system = models.CharField(max_length=20, default='', choices = SYS_CHOICES)
     department = models.CharField(max_length=20, default='', choices = DEPT_CHOICES, blank=True, null=True)
     assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, db_column='assigned_to', blank=True, null=True, on_delete = models.PROTECT, related_name='assigned')
-    status = models.CharField(max_length=20, default='', blank=True, null=True, choices = TICKET_STATUS_CHOICES)
+    status = models.CharField(max_length=20, default='awaiting assignment', blank=True, null=True, choices = TICKET_STATUS_CHOICES)
     priority = models.CharField(max_length=20, default='', blank=True, null=True, choices = PRIORITY_CHOICES)
     datetime = models.DateTimeField(auto_now_add=True)
