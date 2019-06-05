d301e3cc900c60c825ea54c961f973b3146cfbee
Gary <garynobles20@gmail.com>
2019-06-05 08:03:33 +0300

    order default added

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,7 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     summary = models.CharField(max_length=200)
     referenceurl = models.CharField(max_length=255)
-    order = models.IntegerField()
+    order = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return str(self.summary)
